00b45481a1cc73fab3637a5ba40072f459003c36
JamesKang003 <kangjames5@gmail.com>
2023-05-24 22:00:15 +0900

    db connect

--- node-express-course-main/03-task-manager/starter/db/connect.js
+++ node-express-course-main/03-task-manager/starter/db/connect.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://kangjames2003:TkQivEi4gufoBALM@cluster0.uxr3ldq.mongodb.net/";
 
-mongoose.connect(connectionString);
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected TO DB"))
+  .catch((err) => console.log(err));
 //hello world
 //wow I wanna play
