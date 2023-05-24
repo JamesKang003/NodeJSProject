7ba6f9ef2e3ea4600ed923cdb76d84580467fc53
JamesKang003 <kangjames5@gmail.com>
2023-05-24 22:06:28 +0900

    hey

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -2,6 +2,7 @@ require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 // middleware
 app.use(express.json());
 
@@ -19,4 +20,13 @@ app.use("/api/v1/tasks", tasks);
 // app.delete("/api/v1/tasks/:id"); - delete task
 const port = 3000;
 
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
 app.listen(port, console.log(`server is listening on port ${port}...`));
--- node-express-course-main/03-task-manager/starter/db/connect.js
+++ node-express-course-main/03-task-manager/starter/db/connect.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://kangjames2003:TkQivEi4gufoBALM@cluster0.uxr3ldq.mongodb.net/";
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected TO DB"))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
 //hello world
 //wow I wanna play
