2fc531092397f4ae540dd317fce1fc43e923c9a8
JamesKang003 <kangjames5@gmail.com>
2023-05-27 21:42:48 +0900

    why not featured

--- node-express-course-main/04-store-api/starter/controllers/products.js
+++ node-express-course-main/04-store-api/starter/controllers/products.js
@@ -1,13 +1,17 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({
+    name: "vase table",
+  });
 
-  res.status(200).json({ products });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  console.log(req.query);
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
