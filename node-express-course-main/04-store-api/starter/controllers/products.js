fb90bd5bc8005e8b8103659f0d5d987bf8a580f9
JamesKang003 <kangjames5@gmail.com>
2023-05-29 20:39:26 +0900

    wow

--- node-express-course-main/04-store-api/starter/controllers/products.js
+++ node-express-course-main/04-store-api/starter/controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("name");
+  const products = await Product.find({}).sort("-name");
 
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,7 +21,11 @@ const getAllProducts = async (req, res) => {
   }
 
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    result = products.sort(sort);
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
